44216b34e15fef14bd717ca3828b2ddac3487a27
itsoso520 <2141607908@qq.com>
2020-01-11 20:32:41 +0800

    Initial commit

---
+++ src/main/java/top/itsoso/IndexServlet.java
@@ -0,0 +1,23 @@
+package top.itsoso;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Description:
+ *
+ * @author 150552
+ * @date 2020/1/11 20:21
+ */
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("hello.jsp").forward(req,resp);
+    }
+}
