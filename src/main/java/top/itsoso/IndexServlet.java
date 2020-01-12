17d54f73d70b5f0e82f9d9bed45a05a12d57d8a1
itsoso520 <2141607908@qq.com>
2020-01-12 10:25:43 +0800

    servlet

--- src/main/java/top/itsoso/IndexServlet.java
+++ src/main/java/top/itsoso/IndexServlet.java
@@ -18,6 +18,6 @@ public class IndexServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("hello.jsp").forward(req,resp);
+        req.getRequestDispatcher("index.jsp").forward(req,resp);
     }
 }
